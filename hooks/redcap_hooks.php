d665b1341c928af9b2e65fbca1ea636ccdc2efb2
123andy <andy.b.martin@stanford.edu>
2014-06-19 19:09:39 -0700

    moved hooks config file into hooks directory

--- hooks/redcap_hooks.php
+++ hooks/redcap_hooks.php
@@ -6,6 +6,12 @@
 
 	This file should be referenced in the REDCap Control Center under General Configuration : REDCap Hooks
 
+	In this example, this file is placed inside the /hooks/ folder off the base redcap directory.  So:
+	- redcap_v5.x.x
+    - plugins
+    - hooks
+       - this file (and it is referenced in the control center, in my case as /var/redcap/dev/webroot/hooks/redcap_hooks.php)
+
 	Andrew Martin
 	Stanford University
 
@@ -17,11 +23,11 @@
 function redcap_survey_page($project_id, $record, $instrument, $event_id, $group_id, $survey_hash, $response_id)
 {    
 	// Check for a global script that applies to all projects
-	$global_handler_script = dirname(__FILE__) . "/hooks/global/redcap_survey_page.php";
+	$global_handler_script = dirname(__FILE__) . "/global/redcap_survey_page.php";
 	if (file_exists($global_handler_script)) include $global_handler_script;
 
 	// Check for a project-specific script
-	$project_handler_script = dirname(__FILE__) . "/hooks/pid{$project_id}/redcap_survey_page.php";
+	$project_handler_script = dirname(__FILE__) . "/pid{$project_id}/redcap_survey_page.php";
 	if (file_exists($project_handler_script)) include $project_handler_script;	
 }
 
@@ -32,7 +38,7 @@ function redcap_survey_page($project_id, $record, $instrument, $event_id, $group
 # This is a custom-stanford hook but could be extended for other LDAP-based users
 function redcap_custom_verify_username($username) {
 	// Check for a global script that applies to all projects
-	$global_handler_script = dirname(__FILE__) . "/hooks/global/redcap_custom_verify_username.php";
+	$global_handler_script = dirname(__FILE__) . "/global/redcap_custom_verify_username.php";
 	if (file_exists($global_handler_script)) {
 		$result = include_once $global_handler_script;
 		return $result;
